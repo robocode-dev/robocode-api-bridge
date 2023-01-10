746cf1693ee3d814a2364bf0e17a6f7cbbc6919c
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2023-01-10 23:10:51 +0100

    Fixed potential NPE

--- robocode-api/src/main/java/dev/robocode/tankroyale/bridge/ColorMapper.java
+++ robocode-api/src/main/java/dev/robocode/tankroyale/bridge/ColorMapper.java
@@ -5,6 +5,6 @@ import dev.robocode.tankroyale.botapi.Color;
 final class ColorMapper {
 
     public static Color map(java.awt.Color awtColor) {
-        return new Color(awtColor.getRed(), awtColor.getGreen(), awtColor.getBlue());
+        return awtColor == null ? null : new Color(awtColor.getRed(), awtColor.getGreen(), awtColor.getBlue());
     }
 }
