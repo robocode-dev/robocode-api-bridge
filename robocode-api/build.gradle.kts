f1976b31f1e01bdb929c35e8968ad0cd09d6074f
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2024-10-11 20:58:45 +0200

    Updated Robocode version to 0.25.0

--- robocode-api/build.gradle.kts
+++ robocode-api/build.gradle.kts
@@ -12,14 +12,13 @@ repositories {
 }
 
 dependencies {
-    implementation("dev.robocode.tankroyale:robocode-tankroyale-bot-api:0.23.0")
+    implementation("dev.robocode.tankroyale:robocode-tankroyale-bot-api:0.25.0")
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = sourceCompatibility
 }
-
 
 tasks {
     jar {
