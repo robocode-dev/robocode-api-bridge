4fa2b76519b0ec4e34aa0ff78b5bfd1896a237c1
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2024-10-29 21:36:05 +0100

    Reduced AngleConverter

--- robocode-api/src/main/java/dev/robocode/tankroyale/bridge/AngleConverter.java
+++ robocode-api/src/main/java/dev/robocode/tankroyale/bridge/AngleConverter.java
@@ -1,18 +1,15 @@
 package dev.robocode.tankroyale.bridge;
 
 import static java.lang.Math.toRadians;
+import static robocode.util.Utils.normalAbsoluteAngleDegrees;
 
 final class AngleConverter {
 
     public static double toRobocodeHeadingRad(double deg) {
-        return toRadians(normalizeAbsoluteAngle(90.0 - deg));
+        return toRadians(normalAbsoluteAngleDegrees(90.0 - deg));
     }
 
     public static double toRobocodeBearingRad(double deg) {
         return toRadians(-deg);
-    }
-
-    private static double normalizeAbsoluteAngle(double angle) {
-        return (angle %= 360) >= 0 ? angle : (angle + 360);
     }
 }
