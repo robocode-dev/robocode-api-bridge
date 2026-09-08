d16e8886b5a6cd609da495c43691cd1dc5dec0d4
Flemming N. Larsen <flemming.n.larsen@gmail.com>
2026-09-08 20:39:47 +0200

    Fix legacy bot lifecycle compatibility

--- robots-wrapper/src/main/java/Main.java
+++ robots-wrapper/src/main/java/Main.java
@@ -324,7 +324,6 @@ public class Main {
                     "\t\tvar robot = (IBasicRobot) robotClass.getDeclaredConstructor().newInstance();\n" +
                     "\t\t\n" +
                     "\t\tvar peer = new BotPeer(robot, BotInfo.fromFile(\"" + robotClassAndVersion + ".json\"));\n" +
-                    "\t\trobot.setPeer(peer);\n" +
                     "\t\tpeer.start();\n" +
                     "\t}\n" +
                     "}"
